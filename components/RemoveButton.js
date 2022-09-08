eb30b8e0dc6952f45eb3b2c9708411f215c1dbeb
Matthew Otani <112737333+motanisama@users.noreply.github.com>
2022-09-07 15:56:42 -1000

    Test (#8)
    * yeeehaw
    
    * commiting pt2
    
    * save pt2
    
    * yessaah
    
    * removed yarn
    
    * disable eslint
    
    * yessir
    
    * yee
    
    * firebase export
    
    * changed env var
    
    * yee
    
    * tesing checkly
    
    * adding linting back
    
    * changed Capital for linter
    
    * corrected linter
    
    * yessir

--- components/RemoveButton.js
+++ components/RemoveButton.js
@@ -56,7 +56,7 @@ function RemoveButton({ feedbackId }) {
                   </AlertDialogHeader>
 
                   <AlertDialogBody>
-                     Are you sure? You can't undo this action afterwards.
+                     {"Are you sure? You can't undo this action afterwards."}
                   </AlertDialogBody>
 
                   <AlertDialogFooter>
